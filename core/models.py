7bbf5464b52f84ac274785ea39df6efdd653f764
Rasa Hosseinzadeh <rasa.hosseinzadeh@gmail.com>
2016-12-31 19:04:00 +0330

    added Branch and Employee Classes to model, added file name to setting

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    father_name = models.CharField(max_length=255)
+    social_id = models.CharField(max_length=10)
+    branch = models.ForeignKey('Branch', on_delete=models.CASCADE)
+
+class Branch(models.Model):
+    manager=models.ForeignKey(
+        Employee, on_delete = models.SET_NULL, null = True, related_name='+')
+    name=models.CharField(max_length = 255)
+    address=models.CharField(max_length = 255)
