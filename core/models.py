b627f521efd18ff0339ec89105214b60e5645b78
Rasa Hosseinzadeh <rasa.hosseinzadeh@gmail.com>
2017-01-01 11:16:10 +0330

    adding admin

--- core/models.py
+++ core/models.py
@@ -8,7 +8,7 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     father_name = models.CharField(max_length=255)
-    social_id = models.CharField(max_length=10)
+    social_id = models.CharField(max_length=10, unique=True)
     branch = models.ForeignKey('Branch', on_delete=models.CASCADE)
     EMPLOYEE_TYPES = (
         ('au', 'auditor'),
@@ -16,10 +16,20 @@ class Employee(models.Model):
         ('ma', 'manager'),
         ('le', 'legal'),
     )
-    type = models.CharField(max_length=2, choices=EMPLOYEE_TYPES)
+    type = models.CharField(
+        max_length=2, choices=EMPLOYEE_TYPES, default='cashier')
+
 
 class Branch(models.Model):
-    manager=models.ForeignKey(
-        Employee, on_delete = models.SET_NULL, null = True, related_name='+')
-    name=models.CharField(max_length = 255)
-    address=models.CharField(max_length = 255)
+    manager = models.ForeignKey(
+        Employee, on_delete=models.SET_NULL, null=True, related_name='+')
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birthday = models.DateField(
+        verbose_name=None, name=None, auto_now=False, auto_now_add=False)
+    father_name = models.CharField(max_length=255)
