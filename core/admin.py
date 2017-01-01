5af54435a52c4244e93fdc0bb55a5818215c558f
Rasa Hosseinzadeh <rasa.hosseinzadeh@gmail.com>
2017-01-01 17:25:24 +0330

    change Employee class to abstract and remove type

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from core.models import Branch,Employee
+from core.models import *
 # Register your models here.
-admin.site.register(Employee)
+admin.site.register(Manager)
+admin.site.register(Auditor)
+admin.site.register(Jursit)
+admin.site.register(Cashier)
 admin.site.register(Branch)
--- core/models.py
+++ core/models.py
@@ -8,19 +8,21 @@ class Customer(models.Model):
     birthday = models.DateField()
     father_name = models.CharField(max_length=255)
 
+
+class Maintainer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+
 class ATM(models.Model):
     balance = models.IntegerField()
-
-
-class ATMGuy(models.Model):
-    user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='atm_guy')
-    ATM = models.OneToOneField(
-        ATM, on_delete=models.CASCADE, related_name='atm_guy')
+    maintainer = models.ForeignKey(
+        Maintainer, on_delete=models.SET_NULL, related_name="atm_set", null=True)
 
 
 class Greenback(models.Model):
     value = models.IntegerField()
+
 
 class Account(models.Model):
     owner = models.ForeignKey(Customer, on_delete=models.CASCADE)
@@ -29,7 +31,8 @@ class Account(models.Model):
 
 
 class Card(models.Model):
-    card_number = models.CharField(max_length=20,unique=True)
+    card_number = models.CharField(
+        max_length=20, unique=True, primary_key=True)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
 
 
@@ -44,9 +47,9 @@ class WithdrawFromATM(models.Model):
 class CardToCard(models.Model):
     volume = models.IntegerField()
     from_card = models.ForeignKey(
-        Card, on_delete=models.SET_NULL, related_name="card_to_card_from", null=True)
+        Card, on_delete=models.SET_NULL, related_name="card_to_card_from_set", null=True)
     to_card = models.ForeignKey(
-        Card, on_delete=models.SET_NULL, related_name="card_to_card_to", null=True)
+        Card, on_delete=models.SET_NULL, related_name="card_to_card_to_set", null=True)
 
 
 class BillType(models.Model):
@@ -62,36 +65,49 @@ class Bill(models.Model):
 
 
 class Employee(models.Model):
-    AUDITOR = 'au'
-    CASHIER = 'ca'
-    MANAGER = 'ma'
-    LEGAL_EXPERT = 'le'
     user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     father_name = models.CharField(max_length=255)
     social_id = models.CharField(max_length=10, unique=True)
-    branch = models.ForeignKey('Branch', on_delete=models.CASCADE)
-    EMPLOYEE_TYPES = (
-        (AUDITOR, 'auditor'),
-        (CASHIER, 'cashier'),
-        (MANAGER, 'manager'),
-        (LEGAL_EXPERT, 'legal expert'),
-    )
-    type = models.CharField(
-        max_length=2, choices=EMPLOYEE_TYPES, default=CASHIER)
+    branch = models.ForeignKey('Branch', on_delete=models.CASCADE, related_name='+')
+
+    class Meta:
+        abstract = True
+
+
+class Manager(Employee):
+
+    def __str__(self):
+        return "Manager: " + first_name + " " + last_name
+
+
+class Auditor(Employee):
+
+    def __str__(self):
+        return "Manager: " + first_name + " " + last_name
+
+
+class Cashier(Employee):
+
+    def __str__(self):
+        return "Manager: " + first_name + " " + last_name
+
+
+class Jursit(Employee):
+
+    def __str__(self):
+        return "Manager: " + first_name + " " + last_name
 
 
 class Branch(models.Model):
     manager = models.ForeignKey(
-        Employee, on_delete=models.SET_NULL, null=True, related_name='+')
+        Manager, on_delete=models.SET_NULL, null=True, related_name='+')
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
 
 
-
-
-class SystemModel(models.Model):
+class System(models.Model):
     card_production_fee = models.IntegerField(default=0)
     check_production_fee = models.IntegerField(default=0)
 
@@ -103,7 +119,9 @@ class LoanApplication(models.Model):
     auditor_confirmation = models.BooleanField(default=False)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
 
+
 class Loan(models.Model):
     remaining_payment = models.IntegerField()
     interest = models.FloatField()
-    loan_application = models.OneToOneField(LoanApplication, on_delete=models.CASCADE)
+    loan_application = models.OneToOneField(
+        LoanApplication, on_delete=models.CASCADE)
