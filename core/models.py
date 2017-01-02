283bc781b3f2761cf942385972a7c71e6035c713
Rasa Hosseinzadeh <rasa.hosseinzadeh@gmail.com>
2017-01-02 11:02:35 +0330

    add some new models and change some attributes

--- core/models.py
+++ core/models.py
@@ -2,6 +2,11 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class System(models.Model):
+    card_production_fee = models.IntegerField(default=0)
+    check_production_fee = models.IntegerField(default=0)
+
+
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -17,7 +22,8 @@ class Maintainer(models.Model):
 class ATM(models.Model):
     balance = models.IntegerField()
     maintainer = models.ForeignKey(
-        Maintainer, on_delete=models.SET_NULL, related_name="atm_set", null=True)
+        Maintainer, on_delete=models.SET_NULL,
+        related_name="atm_set", null=True)
 
 
 class Greenback(models.Model):
@@ -25,43 +31,37 @@ class Greenback(models.Model):
 
 
 class Account(models.Model):
-    owner = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Customer, on_delete=models.PROTECT)
     account_number = models.IntegerField(primary_key=True)
     balance = models.IntegerField(default=0)
+    is_blocked = models.BooleanField(default=False)
 
 
 class Card(models.Model):
     card_number = models.CharField(
         max_length=20, unique=True, primary_key=True)
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.PROTECT)
 
 
 class WithdrawFromATM(models.Model):
     ATM = models.ForeignKey(ATM, on_delete=models.SET_NULL,
                             related_name="withdrawal", null=True)
     card = models.ForeignKey(
-        Card, on_delete=models.SET_NULL, related_name="withdrawal", null=True)
+        Card, on_delete=models.PROTECT, related_name="withdrawal", null=True)
     volume = models.IntegerField()
 
 
 class CardToCard(models.Model):
     volume = models.IntegerField()
+
     from_card = models.ForeignKey(
-        Card, on_delete=models.SET_NULL, related_name="card_to_card_from_set", null=True)
+        Card, on_delete=models.PROTECT, related_name="from_card", null=True)
+
     to_card = models.ForeignKey(
-        Card, on_delete=models.SET_NULL, related_name="card_to_card_to_set", null=True)
+        Card, on_delete=models.PROTECT, related_name="to_card", null=True)
 
-
-class BillType(models.Model):
-    company = models.CharField(max_length=30)
-    # TODO FK be hesab
-
-
-class Bill(models.Model):
-    bill_type = models.ForeignKey(
-        BillType, on_delete=models.CASCADE, related_name="bills")
-    total = models.IntegerField()
-    # TODO fk to pay transaction
+    atm = models.ForeignKey(ATM, on_delete=models.SET_NULL,
+                            related_name="card_to_card", null=True)
 
 
 class Employee(models.Model):
@@ -70,7 +70,8 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=255)
     father_name = models.CharField(max_length=255)
     social_id = models.CharField(max_length=10, unique=True)
-    branch = models.ForeignKey('Branch', on_delete=models.CASCADE, related_name='+')
+    branch = models.ForeignKey(
+        'Branch', on_delete=models.CASCADE, related_name='+')
 
     class Meta:
         abstract = True
@@ -102,26 +103,91 @@ class Jursit(Employee):
 
 class Branch(models.Model):
     manager = models.ForeignKey(
-        Manager, on_delete=models.SET_NULL, null=True, related_name='+')
+        Manager, on_delete=models.PROTECT, null=True, related_name='+')
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
 
 
-class System(models.Model):
-    card_production_fee = models.IntegerField(default=0)
-    check_production_fee = models.IntegerField(default=0)
+class Transaction(models.Model):
+    amount = models.IntegerField()
+    account = models.ForeignKey(Account, on_delete=models.PROTECT)
+    cashier = models.ForeignKey(
+        Auditor, on_delete=models.SET_NULL,null =True, related_name='transactions')
 
+    date = models.DateField(auto_now=True)
+    time = models.TimeField(auto_now=True)
+    wage_transaction = models.ForeignKey(
+        'self', on_delete=models.PROTECT, related_name='+')
+    WITHDRAW = 'w'
+    DEPOSIT = 'd'
+    transaction_types = (
+        (WITHDRAW, 'withdraw'),
+        (DEPOSIT, 'deposit'),
+    )
+
+    transaction_type = models.CharField(
+        max_length=1, choices=transaction_types, db_column='type')
+
+    # TODO: why there should be a foreign key to payment orders
+
+
+class Transfer(models.Model):
+    withdraw = models.ForeignKey(
+        Transaction, on_delete=models.PROTECT, related_name='+')
+    deposit = models.ForeignKey(
+        Transaction, on_delete=models.PROTECT, related_name='+')
+
+
+
+class BillType(models.Model):
+    company = models.CharField(max_length=30)
+    account = models.ForeignKey(
+        Account, on_delete=models.PROTECT, related_name="bill", null=True)
+
+
+class Bill(models.Model):
+    bill_type = models.ForeignKey(
+        BillType, on_delete=models.PROTECT, related_name="bills")
+    amount = models.IntegerField()
+    payment = models.ForeignKey(
+        Transfer, on_delete=models.PROTECT, related_name='+')
 
 class LoanApplication(models.Model):
     balance = models.BigIntegerField()
     payment_count = models.IntegerField()
     legal_expert_confirmation = models.BooleanField(default=False)
     auditor_confirmation = models.BooleanField(default=False)
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.PROTECT)
 
 
 class Loan(models.Model):
     remaining_payment = models.IntegerField()
     interest = models.FloatField()
-    loan_application = models.OneToOneField(
-        LoanApplication, on_delete=models.CASCADE)
+    loan_application = models.ForeignKey(
+        LoanApplication, on_delete=models.PROTECT)
+
+
+class Inquiry(models.Model):
+    UNKNOWN = 'NA'
+    ACCEPT = 'AC'
+    REJECT = 'RE'
+    statuses = (
+        (UNKNOWN, 'unknown'),
+        (ACCEPT, 'accepted'),
+        (REJECT, 'rejected'),
+    )
+    status = models.CharField(
+        max_length=2, choices=statuses, default=UNKNOWN)
+
+
+class Cheque(models.Model):
+    account = models.ForeignKey(
+        Account, on_delete=models.PROTECT, related_name="cheques")
+
+
+class PaymentOrder(models.Model):  # NOTE: i don't know what is english for "havale"
+    account = models.ForeignKey(
+        Account, on_delete=models.PROTECT, related_name="payment_orders")
+    amount = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
